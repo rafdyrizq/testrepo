c7ee335b25dfb252376dfee993f462c450b16291
Rizq Abi Rafdy <74049658+rafdyrizq@users.noreply.github.com>
2022-01-12 17:29:15 +0700

    Add files via upload

---
+++ TM1.py
@@ -0,0 +1,38 @@
+# #print('Hello World')
+
+# # ctrl+/ untuk comment
+
+# print('Halo Dunia')
+
+# # Variable
+# x = 'Halo Dunia'
+# print(x)
+# y = 22
+# z = 1.51
+# print(y)
+# print(z)
+
+# a = True 
+
+# Data Type
+
+# type() untuk ngecek data type
+
+# print(type('Halo Dunia'))
+# print(type(2))
+# print(type(1.5))
+# print(type(True))
+
+# Input
+
+# print(input('Coba tulis apapun di sini!'))
+
+# x = input('Masukkan angka!')
+# print(x)
+
+# print(type(x))
+
+# x = int(input('Input Angka di sini'))
+# print(x)
+# print(type(x))
+
